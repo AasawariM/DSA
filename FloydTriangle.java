ccd7b85138c1b05fd705c0adff3ba52c7de2f886
AasawariM <aasawari.m.work@gmail.com>
2026-01-20 00:49:33 +0530

    Floyd's Triangle

---
+++ FloydTriangle.java
@@ -0,0 +1,16 @@
+public class FloydTriangle {
+  public static void main(String[] args) {
+
+    // Number Changing pyramid
+    int n = 5;
+    int i, j;
+    int number = 1;
+    for (i = 1; i <= n; i++) {
+      for (j = 1; j <= i; j++) {
+        System.out.print(number + " ");
+        number++;
+      }
+      System.out.println();
+    }
+  }
+}
